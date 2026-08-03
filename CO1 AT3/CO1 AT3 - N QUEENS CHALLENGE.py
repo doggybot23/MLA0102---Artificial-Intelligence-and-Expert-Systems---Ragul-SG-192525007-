2e43a6065a7f61f7a539964dfc3cc92c796b224c
doggybot <ragulsg23@gmail.com>
2026-08-03 14:14:59 +0530

    Update CO1 AT3 - N QUEENS CHALLENGE.py

--- CO1 AT3/CO1 AT3 - N QUEENS CHALLENGE.py
+++ CO1 AT3/CO1 AT3 - N QUEENS CHALLENGE.py
@@ -1,63 +1,41 @@
-# N-Queens Challenge (12 x 12)
-# Backtracking Algorithm
-
-N = 12
-
-board = [[0 for _ in range(N)] for _ in range(N)]
-
-def is_safe(board, row, col):
-    # Check left side of current row
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-
-    # Check upper-left diagonal
-    i, j = row, col
-    while i >= 0 and j >= 0:
-        if board[i][j] == 1:
-            return False
-        i -= 1
-        j -= 1
-
-    # Check lower-left diagonal
-    i, j = row, col
-    while i < N and j >= 0:
-        if board[i][j] == 1:
-            return False
-        i += 1
-        j -= 1
-
-    return True
-
-def solve(board, col):
-    if col >= N:
+def run_12_queens():
+    print("\n" + "="*30)
+    print("--- 12-Queens (Backtracking) ---")
+    print("="*30)
+    print("# 12-Queens:")
+    print("# Q = Queen")
+    print("# . = Empty Square")
+    print("="*30 + "\n")
+    
+    N = 12
+    board = [[0] * N for _ in range(N)]
+    
+    def is_safe(r, c):
+        for i in range(c):
+            if board[r][i] == 1: return False
+        for i, j in zip(range(r, -1, -1), range(c, -1, -1)):
+            if board[i][j] == 1: return False
+        for i, j in zip(range(r, N, 1), range(c, -1, -1)):
+            if board[i][j] == 1: return False
         return True
+        
+    def solve(col):
+        if col >= N: return True
+        for i in range(N):
+            if is_safe(i, col):
+                board[i][col] = 1
+                if solve(col + 1):
+                    return True
+                board[i][col] = 0 # Backtrack
+        return False
+        
+    print("Searching for a solution...")
+    if solve(0):
+        print(f"\nSolution for {N}-Queens:")
+        for row in board:
+            print("  " + " ".join("Q" if x == 1 else "." for x in row))
+    else:
+        print("No solution exists.")
 
-    for row in range(N):
-        if is_safe(board, row, col):
-            board[row][col] = 1
-
-            if solve(board, col + 1):
-                return True
-
-            board[row][col] = 0
-
-    return False
-
-if solve(board, 0):
-    print("12-Queens Solution:\n")
-
-    moves = 0
-    for i in range(N):
-        for j in range(N):
-            if board[i][j] == 1:
-                print("Q", end=" ")
-                moves += 1
-            else:
-                print(".", end=" ")
-        print()
-
-    print("\nQueens Placed:", moves)
-    print("Conflicts: 0")
-else:
-    print("No solution exists.")
+if __name__ == "__main__":
+    run_12_queens()
